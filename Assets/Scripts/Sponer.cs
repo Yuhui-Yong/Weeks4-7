138dbb3c1e2e6fb1ba2ffc70b5ed174d4c52cbf1
Yuhui Yong <yongy@shernet.sheridancollege.ca>
2026-02-03 15:41:41 -0500

    I am done with week 4 Coding Gym

---
+++ Assets/Scripts/Sponer.cs
@@ -0,0 +1,27 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Sponer : MonoBehaviour
+{
+    public GameObject Buddy;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Click()
+    {
+            Vector2 spawnPos = Random.insideUnitCircle * 3;
+            Instantiate(Buddy, spawnPos, Quaternion.identity);
+    }
+
+}
