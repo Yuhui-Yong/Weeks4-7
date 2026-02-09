b061a32aa31a4cf99e5f0f64a3ef0a152eeedbfa
Yuhui Yong <yongy@shernet.sheridancollege.ca>
2026-02-09 18:54:58 -0500

    The ToyTask assignment.

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public float timerValue = 0;
+    public float timerMaxValue = 12;
+
+    public Slider timeVisuals;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timeVisuals.maxValue = timerMaxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerValue += Time.deltaTime;
+
+        if (timerValue > timerMaxValue)
+        {
+            timerValue = 0;
+        }
+
+        timeVisuals.value = timerValue;
+    }
+}
