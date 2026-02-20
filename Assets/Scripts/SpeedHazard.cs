b22ee2abc7ebafd36771739f8dfa4240f0688ecf
Yuhui Yong <yongy@shernet.sheridancollege.ca>
2026-02-20 17:02:31 -0500

    Week7 during Class stuff.

---
+++ Assets/Scripts/SpeedHazard.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SpeedHazard : MonoBehaviour
+{
+    public Player player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AookySlowdown()
+    {
+        // PlayerApplySlowdown(slowdownValue);
+    }
+
+    public void ResetSpeed()
+    {
+        // Player.ResetSpeed();
+    }
+
+}
