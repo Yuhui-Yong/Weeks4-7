d1b9817abdfadcc4bcd70cc0bdbb930a313f8d38
Yuhui Yong <yongy@shernet.sheridancollege.ca>
2026-03-10 18:55:09 -0400

    Almost finished, just gotta figure out how to respawn and make it works with KeelyObjects with like Prefab thing or something.

--- Assets/Scripts/DamageHazard.cs
+++ Assets/Scripts/DamageHazard.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 public class DamageHazard : MonoBehaviour
 {
     public Color damageColour;
     private Color playerColour;
 
+    public Sprite Yuhui;
+    public Sprite YuhuiDamage;
+
     public int damage;
     public Player player;
+
+    private float speed = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +22,30 @@ public class DamageHazard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool leftArrowHeld = Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed;
+        bool rightArrowHeld = Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed;
+        bool upArrowHeld = Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed;
+        bool downArrowHeld = Keyboard.current.downArrowKey.isPressed || Keyboard.current.sKey.isPressed;
 
+        if (leftArrowHeld)
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if (rightArrowHeld)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if (upArrowHeld)
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        if (downArrowHeld)
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
     }
 
     public void ApplyDamage()
@@ -24,16 +53,29 @@ public class DamageHazard : MonoBehaviour
         player.TakeDamage(damage);
     }
 
-    public void ChangePlayerColour()
+    public void ChangePlayerColor()
     {
         SpriteRenderer playerRenderer = player.GetComponent<SpriteRenderer>();
         playerColour = playerRenderer.color;
         playerRenderer.color = damageColour;
     }
 
-    public void ResetPlayerColour()
+    public void OriginalPlayerColour()
     {
         SpriteRenderer playerRenderer = player.GetComponent<SpriteRenderer>();
         playerRenderer.color = playerColour;
+    }
+
+    public void ChangeYuhuiSprite()
+    {
+        SpriteRenderer playerRenderer = player.GetComponent<SpriteRenderer>();
+        Yuhui = playerRenderer.sprite;
+        playerRenderer.sprite = YuhuiDamage;
+    }
+
+    public void OriginalYuhuiSprite()
+    {
+        SpriteRenderer playerRenderer = player.GetComponent<SpriteRenderer>();
+        playerRenderer.sprite = Yuhui;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public int health;
+    public GameObject YuhuiPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +15,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+
+        Vector3 spawnPos = new Vector3(6.07f, 0f, 0f);
+        if (Mouse.current.leftButton.wasPressedThisFrame == true)
+        {
+            Instantiate(gameObject, spawnPos, Quaternion.identity);
+        }
     }
     public void TakeDamage(int damage)
     {
